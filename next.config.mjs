bb9bdb6c44bad09fc2fcaf64a25cabf4c11badf7
anclaev <iahugo@yandex.ru>
2024-05-17 04:38:28 +0300

    Фикс Dockerfile.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  /** For GitHub Pages */
-   basePath: process.env.CI === 'pages' ? '/spomen-client' : '',
-   assetPrefix: process.env.CI === 'pages' ? '/spomen-client/' : '/'
-};
+  swcMinify: true,
+  output: 'standalone',
+}
 
-export default nextConfig;
+export default nextConfig
