1e605884a62db94a62380f33bdbab33e3b4edb5f
anclaev <iahugo@yandex.ru>
2024-06-11 16:27:12 +0300

    Валидация полей страниц логина.

--- src/app/sign-in/sign-in-callback/sign-in-callback.component.ts
+++ src/app/sign-in/sign-in-callback/sign-in-callback.component.ts
@@ -1,22 +1,44 @@
-import { Component, OnInit, inject } from '@angular/core'
+import { Component, OnDestroy, OnInit, inject } from '@angular/core'
+import { BehaviorSubject, Subscription } from 'rxjs'
+import { TuiAlertService } from '@taiga-ui/core'
 import { ActivatedRoute } from '@angular/router'
-import { BehaviorSubject } from 'rxjs'
+import { CommonModule } from '@angular/common'
+
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms'
 
 import { getQueryPayload } from '@utils/getQueryPayload'
+import Validation from '@utils/validation'
 
 import { SignInCallbackResponse } from '@tps/dto/sign-in-callback'
 
 @Component({
   selector: 'spomen-sign-in-callback',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sign-in-callback.component.html',
   styleUrl: './sign-in-callback.component.scss',
 })
-export class SignInCallbackComponent implements OnInit {
-  private route = inject(ActivatedRoute)
-
+export class SignInCallbackComponent implements OnInit, OnDestroy {
   private token: BehaviorSubject<string> = new BehaviorSubject('')
+  private route = inject(ActivatedRoute)
+  private alerts = inject(TuiAlertService)
+  private subs$: Subscription[] = []
+
+  form: FormGroup<{
+    pass: FormControl<string | null>
+    confirmPass: FormControl<any>
+  }> = new FormGroup({
+    pass: new FormControl(''),
+    confirmPass: new FormControl(),
+  })
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     const payload = getQueryPayload<SignInCallbackResponse>(
@@ -24,5 +46,72 @@ export class SignInCallbackComponent implements OnInit {
     )!
 
     this.token.next(payload.token)
+
+    this.form = this.fb.group(
+      {
+        pass: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.maxLength(30),
+          ],
+        ],
+        confirmPass: ['', [Validators.required]],
+      },
+      {
+        validators: [Validation.match('pass', 'confirmPass')],
+      }
+    )
+  }
+
+  onSubmit() {
+    if (!this.form.valid) {
+      let errors: string[] = []
+
+      Object.keys(this.form.controls).forEach((control) => {
+        if (
+          this.form.controls[control as keyof typeof this.form.controls].errors
+        ) {
+          Object.keys(
+            this.form.controls[control as keyof typeof this.form.controls]
+              .errors!
+          ).forEach((error) => {
+            switch (error) {
+              case 'required': {
+                errors.push('Пароль обязателен')
+                break
+              }
+
+              case 'minlength': {
+                errors.push('Пароль должен быть не менее 8 символов')
+                break
+              }
+
+              case 'match': {
+                errors.push('Пароли должны быть одинаковы')
+                break
+              }
+
+              default: {
+                errors.push('Проверьте правильность полей')
+              }
+            }
+          })
+        }
+      })
+
+      new Set(errors).forEach((error) =>
+        this.subs$.push(this.alerts.open(error).subscribe())
+      )
+
+      return
+    }
+
+    this.subs$.push(this.alerts.open('Запрос на сервер...').subscribe())
+  }
+
+  ngOnDestroy(): void {
+    this.subs$.forEach((sub) => sub.unsubscribe())
   }
 }
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,17 +1,70 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core'
+import { TuiAlertService } from '@taiga-ui/core'
+import { CommonModule } from '@angular/common'
+import { Subscription } from 'rxjs'
 import * as VKID from '@vkid/sdk'
+
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  inject,
+} from '@angular/core'
+
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms'
 
 @Component({
   selector: 'spomen-sign-in',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
 })
-export class SignInComponent implements AfterViewInit {
+export class SignInComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('VkIdSdkOneTap') VkIdSdkOneTap!: ElementRef<HTMLDivElement>
 
   private vkIdOneTap = new VKID.OneTap()
+  private alerts = inject(TuiAlertService)
+  private subs$: Subscription[] = []
+
+  form: FormGroup<{
+    login: FormControl<string | null>
+    pass: FormControl<string | null>
+  }> = new FormGroup({
+    login: new FormControl(''),
+    pass: new FormControl(''),
+  })
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      login: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(30),
+        ],
+      ],
+      pass: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(30),
+        ],
+      ],
+    })
+  }
 
   ngAfterViewInit(): void {
     if (this.VkIdSdkOneTap) {
@@ -24,5 +77,31 @@ export class SignInComponent implements AfterViewInit {
         },
       })
     }
+  }
+
+  onSubmit() {
+    if (!this.form.valid) {
+      let errors: string[] = []
+
+      if (this.form.controls.login.errors) {
+        errors.push('Введите логин')
+      }
+
+      if (this.form.controls.pass.errors) {
+        errors.push('Введите пароль')
+      }
+
+      errors.forEach((error) =>
+        this.subs$.push(this.alerts.open(error).subscribe())
+      )
+
+      return
+    }
+
+    this.subs$.push(this.alerts.open('Запрос на сервер...').subscribe())
+  }
+
+  ngOnDestroy(): void {
+    this.subs$.forEach((sub) => sub.unsubscribe())
   }
 }
