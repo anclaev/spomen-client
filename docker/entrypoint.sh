655d289ffe9b0f3eb831c7dc4606c3c6f6b785ba
anclaev <iahugo@yandex.ru>
2024-06-30 04:23:17 +0300

    Фикс CI.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cat <<EOT >> /usr/share/nginx/html/assets/config.json
 {
-  \"appId\": \"$VKID_APP_ID\",
-  \"redirectUrl\": \"$VKID_REDIRECT_URL\",
-  \"apiUrl\": \"$API_URL\",
+  \"appId\": \"$VK_ID_APP_ID\",
+  \"redirectUrl\": \"$VK_ID_REDIRECT_URL\",
+  \"apiUrl\": \"$API_HOST\",
   {\"origin\": \"$ORIGIN\"}
 }
 EOT
