27ea5cd6cb5b3f01560ee7dc6f0f918a8622b31c
anclaev <iahugo@yandex.ru>
2024-07-02 14:27:54 +0300

    Фикс CI. [ci]

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,8 @@ echo "
   \"redirectUrl\": \"$VK_ID_REDIRECT_URL\",
   \"apiUrl\": \"$API_URL\",
   \"origin\": \"$ORIGIN\",
-  \"sentryDsn\": \"$SENTRY_DSN\"
+  \"sentryDsn\": \"$SENTRY_DSN\",
+  \"environment\": \"$NODE_ENV\"
 }
 " > /usr/share/nginx/html/assets/config.json
 
